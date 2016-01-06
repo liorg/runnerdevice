2e5d0bf596aea2c8b43fdc2e84991c940170a558
lior_gr <lior_gr@LAP-GS-92.jer.ad.malam.com>
2016-01-06 17:46:42 +0200

    ok get work

--- src/il/co/runnerdevice/MainActivity.java
+++ src/il/co/runnerdevice/MainActivity.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
@@ -17,9 +18,11 @@ import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
 import org.json.JSONObject;
 
-
+import org.json.JSONObject;
 
 
 
@@ -35,30 +38,41 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
-import android.os.Bundle;
+
 import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
+//http://www.techrepublic.com/blog/software-engineer/calling-restful-services-from-your-android-app/
 
+//https://zackehh.com/andrest-simple-rest-client-implementation-android/
 //http://avilyne.com/?p=105
-public class MainActivity extends ActionBarActivity {
+public class MainActivity  extends Activity implements OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.my_button).setOnClickListener(this);
     }
+    public void onClick(View arg0) {
+    	Button b = (Button)findViewById(R.id.my_button);
 
+
+    	b.setClickable(false);
+    	new LongRunningGetIO().execute();
+    	}
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-
+   
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -70,4 +84,47 @@ public class MainActivity extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+    
+private class LongRunningGetIO extends AsyncTask <Void, Void, String> {
+		
+		protected String getASCIIContentFromEntity(HttpEntity entity) throws IllegalStateException, IOException {
+	       InputStream in = entity.getContent();
+	         StringBuffer out = new StringBuffer();
+	         int n = 1;
+	         while (n>0) {
+	             byte[] b = new byte[4096];
+	             n =  in.read(b);
+	             if (n>0) out.append(new String(b, 0, n));
+	         }
+	         return out.toString();
+	    }
+		
+		@Override
+		protected String doInBackground(Void... params) {
+			 HttpClient httpClient = new DefaultHttpClient();
+			 HttpContext localContext = new BasicHttpContext();
+             HttpGet httpGet = new HttpGet("http://www.cheesejedi.com/rest_services/get_big_cheese.php?puzzle=1");
+             String text = null;
+             try {
+                   HttpResponse response = httpClient.execute(httpGet, localContext);
+                   HttpEntity entity = response.getEntity();
+                   text = getASCIIContentFromEntity(entity);
+             } catch (Exception e) {
+            	 return e.getLocalizedMessage();
+             }
+             return text;
+		}	
+		
+		protected void onPostExecute(String results) {
+			if (results!=null) {
+				EditText et = (EditText)findViewById(R.id.my_edit);
+				et.setText(results);
+			}
+			Button b = (Button)findViewById(R.id.my_button);
+			b.setClickable(true);
+		}
+    }
 }
+
+
+
