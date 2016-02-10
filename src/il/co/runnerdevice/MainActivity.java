2639fa5043c649f816bff6a34925c2bd5fa90a43
lior grossman <lior.grosman@gmail.com>
2016-02-10 18:53:01 +0200

    ok

--- src/il/co/runnerdevice/MainActivity.java
+++ src/il/co/runnerdevice/MainActivity.java
@@ -22,6 +22,18 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.res.TypedArray;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+
 public class MainActivity extends Activity {
 
 	String[] menutitles;
@@ -133,7 +145,7 @@ Toast.makeText(getApplicationContext(), "User Login Status: " + session.isLogged
 		Fragment fragment = null;
 		switch (position) {
 		case 0:
-			fragment = new TB_Fragment();
+			fragment = new MyShip_Fragment();
 			
 			break;
 		case 1:
