849082fcd5364bd0f89c53992466e0e815b6292a
lior grossman <lior.grosman@gmail.com>
2016-02-18 01:47:03 +0200

    and also ...

---
+++ src/il/co/runnerdevice/Tab2Fragment.java
@@ -0,0 +1,53 @@
+package il.co.runnerdevice; 
+
+//import android.app.Fragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTabHost;
+import android.support.v4.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class Tab2Fragment extends Fragment  implements OnClickListener{
+	ShipItemView shipItem;
+	SessionManager session;
+	//public Tab2Fragment(ShipItemView shipItemView,SessionManager sessionManager)
+	//{
+	//	super();
+		//shipItem=shipItemView;
+		//session=sessionManager;
+	//}
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+	    View v = inflater.inflate(R.layout.tab2, container, false);
+	    Button button=(Button)v.findViewById(R.id.btn_frag2);//
+	   TextView txt=(TextView)v.findViewById(R.id.textView2);//
+	    
+	 Bundle args=   getArguments();  
+	 String status=args.getString("status", "");
+	 txt.setText(status);
+	 
+	    button.setOnClickListener(this);
+	    //	return (LinearLayout) inflater.inflate(R.layout.tab2, container, false);
+	    return  (LinearLayout)v;
+	}
+	
+	
+	
+	public void onClick(View view) {
+	    MyShip_Fragment fragment2 = new MyShip_Fragment();
+	    FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+	    FragmentTransaction fragmentTransaction =        fragmentManager.beginTransaction();
+	    fragmentTransaction.replace(R.id.frame_container, fragment2);
+	    fragmentTransaction.addToBackStack(null);
+	    fragmentTransaction.commit();
+	}
+
+}
